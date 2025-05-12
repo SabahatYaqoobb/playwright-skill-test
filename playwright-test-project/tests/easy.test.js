c66d2d210ee4327df1f096b98b1ad0f58a97deb8
Sabahat Yaqoob <87861506+SabahatYaqoobb@users.noreply.github.com>
2025-05-12 21:05:38 +0200

    Delete playwright-test-project directory

--- playwright-test-project/tests/easy.test.js
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('Easy: Navigate and check heading', async ({ page }) => {
-  await page.goto('https://example.com');
-  await page.click('text=More information');
-  await expect(page).toHaveURL(/iana.org/);
-  await expect(page.locator('h1')).toContainText('IANA');
-});
